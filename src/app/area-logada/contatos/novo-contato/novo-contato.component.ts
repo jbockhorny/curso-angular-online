aa905e54542c608fda417559bf55a2e4fda8b3cd
jbockhorny <jbockhorny@gmail.com>
2020-05-16 11:35:40 -0300

    feat: Aplicando validação nos campos

--- src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
+++ src/app/area-logada/contatos/novo-contato/novo-contato.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-contato',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoContatoComponent implements OnInit {
 
-  constructor() { }
+  contatoForm: FormGroup;
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // this.contatoForm = new FormGroup({
+    //   nome: new FormControl(),
+    //   banco: new FormControl(),
+    // });
+
+    this.contatoForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, ]],
+      banco: ['', [Validators.required, Validators.minLength(3)]],
+    });
+  }
+
+  // voltar(){
+  //   this.router.navigate(['contatos']);
+  // }
+
+  salvarContato(){
+    console.log(this.contatoForm);
   }
 
 }
